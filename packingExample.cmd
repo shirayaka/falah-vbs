d0bca492026e791d540803ed06443904dcc4eac3
petr.macha <petr.macha@bisimulations.com>
2023-10-26 11:55:21 +0700

    packing example

---
+++ packingExample.cmd
@@ -0,0 +1,2 @@
+P:\tools\blue\AddonPacker_64\AddonPacker_x64.exe -s -ap -ac -p FALAH -r -rt -odi -o o:\VBS4-release-22.2.x\data\BlueBase\content\core\addons\plugins\ -i P:\vbs2\customer\plugins\falah_behaviors\
+pause
